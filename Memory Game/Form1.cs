d0fadf26c62804af2c8d27e806cd45603dcdedaf
gabi-sudo <82399945+gabi-sudo@users.noreply.github.com>
2021-08-29 14:49:28 +0200

    Add files via upload

---
+++ Memory Game/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory_Game
+{
+    public partial class Form1 : Form
+    {
+        Label firstclicked = null;
+        Label secondclicked = null;
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "🍎","🍎","🍐","🍐","🍊","🍊","🍌","🍌",
+            "🍓","🍓","🍒","🍒","🍑","🍑","🍍","🍍"
+        };
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconlabel = control as Label;
+                if (iconlabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconlabel.Text = icons[randomNumber];
+                    iconlabel.ForeColor = iconlabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void clickImg(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+            Label clickedLabel = sender as Label;
+            if (clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+                if (firstclicked == null)
+                {
+                    firstclicked = clickedLabel;
+                    clickedLabel.ForeColor = Color.Black;
+                    return;
+                }
+                secondclicked = clickedLabel;
+                clickedLabel.ForeColor = Color.Black;
+                winner();
+                if (firstclicked.Text == secondclicked.Text)
+                {
+                    firstclicked = null;
+                    secondclicked = null;
+                    return;
+                }
+                timer1.Start();
+            }
+        }
+        private void winner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconlabel = control as Label;
+                if (iconlabel != null)
+                {
+                    if (iconlabel.ForeColor == iconlabel.BackColor)
+                        return;
+                }
+            }
+            MessageBox.Show("Победи!");
+            Close();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            firstclicked.ForeColor = firstclicked.BackColor;
+            secondclicked.ForeColor = secondclicked.BackColor;
+            firstclicked = null;
+            secondclicked = null;
+        }
+    }
+}
